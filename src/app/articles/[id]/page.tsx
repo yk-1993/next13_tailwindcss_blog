f9ecbef186eac57a39c315430d0c406bf73c3461
YusukeKimura <>
2024-03-20 15:01:57 +0900

    style調整

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -9,7 +9,7 @@ const Article = async ({ params }: { params: { id: string } }) => {
   const detailArticle = await res.json();
 
   return (
-    <div className="max-w-3xl mx-auto p-5">
+    <div className="max-w-3xl mx-auto p-4">
       <Image
         src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`}
         alt=""
